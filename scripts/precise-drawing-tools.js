282abc4e5ccaa71b68baf2c0ae6164ccba0ea036
itamar <itamarcu@gmail.com>
2024-07-11 09:20:56 +0300

    Fix drag-resistance setting not working after scene changes (#8)

--- scripts/precise-drawing-tools.js
+++ scripts/precise-drawing-tools.js
@@ -159,6 +159,9 @@ Hooks.once('setup', function () {
 
 Hooks.once('canvasReady', function () {
   Hooks.on('renderSceneControls', updateActiveToolLimits)
+})
+
+Hooks.on('canvasReady', function () {
   canvas.stage.on('mousedown', handleMouseDown)
   canvas.stage.on('mouseup', handleMouseUp)
   canvas.stage.on('mousemove', handleMouseMove)
